3376d907a5e6296a0cdb5829d794e32a855bb423
Kamil Łucyszyn <klucyszyn1995@gmail.com>
2020-01-19 18:12:27 +0100

    add gulp-copy; implement basic coping'

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,30 +1,37 @@
-var {src, dest, series, parallel} = require('gulp');
-var log = require('fancy-log');
+const { src, dest, series, parallel } = require('gulp');
+const copy = require('gulp-copy');
+const log = require('fancy-log');
 
-function cleanTask(cb){
+function cleanTask(cb) {
     log('Cleaning...');
-    cb();
+    cb(); 
 }
 
-function jsTask(cb){
+function jsTask(cb) {
     log('Running js task!');
     cb();
 }
 
-function cssTask(cb){
+function cssTask(cb) {
     log('Running css task!');
-    log('sdfa')
-    
+    log('sdfa')    
     cb();
 }
 
-
+function copyTexts() {
+    let csrc = ['src/**/*.txt'];
+    let cdest = 'output';
+    let options = { prefix: 2 };
+    log('Copying files from ' + csrc + ' to output');
+    return src(csrc)
+        .pipe(copy(cdest, options));
+}
 
 exports.clean = cleanTask;
-exports.build = parallel(jsTask, cssTask);
+exports.build = parallel(jsTask, cssTask)
 exports.rebuild = series(
     cleanTask,
     parallel(jsTask, cssTask)
-)
+);
 // the default task is build task
-exports.default = parallel(jsTask, cssTask);
+exports.copy = copyTexts;
